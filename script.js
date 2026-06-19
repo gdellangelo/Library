d664e61e88661845fdbf0ea52c4aa22e6806bfd8
Giovanni Dell'Angelo <dellangelo.giovanni@gmail.com>
2026-06-19 16:13:58 +0200

    create basic flow: input --> new object --> new card --> card render

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ const overlay = document.querySelector(".overlay");
 const bookForm = document.querySelector("#bookForm");
 const cancelFormBtn = document.querySelector("#cancel");
 const inputTitle = document.querySelector("#title");
+const inputPages = document.querySelector("#pages");
 const gridBox = document.querySelector(".grid");
 
 const Books = [];
+
+function Book(title, pages) {
+    this.title = title;
+    this.pages = pages
+}
 
 function renderBookLibrary() {
     gridBox.textContent = "";
@@ -13,6 +19,15 @@ function renderBookLibrary() {
     Books.forEach(function (element) {
         const card = document.createElement("div");
         card.classList.add("card");
+
+        const title = document.createElement("h1");
+        title.textContent = element.title;
+
+        const pages = document.createElement("p");
+        pages.textContent = element.pages
+
+        card.appendChild(pages);
+        card.appendChild(title);
         gridBox.appendChild(card);
     });
 }
@@ -32,7 +47,9 @@ cancelFormBtn.addEventListener("click", cancelForm);
 function addNewBook(event) {
     event.preventDefault();
 
-    Books.push(inputTitle.value);
+    const newBook = new Book(inputTitle.value, inputPages.value);
+
+    Books.push(newBook);
     console.log(Books);
 
     cancelForm();
