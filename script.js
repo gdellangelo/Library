b229827a50c8337d786fdc170c2e9c2419318d74
Giovanni Dell'Angelo <dellangelo.giovanni@gmail.com>
2026-06-19 08:44:50 +0200

    Build initial library UI and form overlay

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
 const newBookBtn = document.querySelector("#newBook");
-const inputForm = document.querySelector(".hidden");
-const cancelBtn = document.querySelector("#cancel");
+const overlay = document.querySelector(".overlay");
+const bookForm = document.querySelector("#bookForm");
+const cancelFormBtn = document.querySelector("#cancel");
+const inputTitle = document.querySelector("#title");
+const gridBox = document.querySelector(".grid");
+
+const Books = [];
+
+function renderBookLibrary() {
+    gridBox.textContent = "";
+
+    Books.forEach(function (element) {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        gridBox.appendChild(card);
+    });
+}
+
+function showForm() {
+    overlay.classList.remove("hidden");
+}
 
 newBookBtn.addEventListener("click", showForm);
 
-function showForm() {
-    inputForm.classList.remove("hidden");
-}
-
-cancelBtn.addEventListener("click", cancelForm);
-
 function cancelForm() {
-    inputForm.classList.add("hidden");
+    overlay.classList.add("hidden");
 }
 
+cancelFormBtn.addEventListener("click", cancelForm);
 
+function addNewBook(event) {
+    event.preventDefault();
+
+    Books.push(inputTitle.value);
+    console.log(Books);
+
+    cancelForm();
+    renderBookLibrary();
+}
+
+bookForm.addEventListener("submit", addNewBook);
