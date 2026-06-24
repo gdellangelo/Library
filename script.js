f6ad0098a7e136fcfcf64c28523a4343cfb6f685
Giovanni Dell'Angelo <dellangelo.giovanni@gmail.com>
2026-06-24 08:59:42 +0200

    add card layout and basic card adding flow

--- script.js
+++ script.js
@@ -1,59 +1,85 @@
-const newBookBtn = document.querySelector("#newBook");
+const addBookBtn = document.querySelector("#addBookBtn");
+const cancelFormBtn = document.querySelector("#cancelFormBtn");
+const submitFormBtn = document.querySelector("#submitFormBtn")
+
 const overlay = document.querySelector(".overlay");
-const bookForm = document.querySelector("#bookForm");
-const cancelFormBtn = document.querySelector("#cancel");
+const form = document.querySelector("form");
+
 const inputTitle = document.querySelector("#title");
+const inputAuthor = document.querySelector("#author");
 const inputPages = document.querySelector("#pages");
-const gridBox = document.querySelector(".grid");
+const inputRead = document.querySelector("#read");
 
-const Books = [];
+const gridBox = document.querySelector(".grid-box");
 
-function Book(title, pages) {
-    this.title = title;
-    this.pages = pages
+const books = [];
+
+function Book(name, author, pages, read) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+};
+
+function hideOverlay() {
+    overlay.classList.add("hidden");
 }
 
-function renderBookLibrary() {
+function showOverlay() {
+    overlay.classList.remove("hidden");
+}
+
+addBookBtn.addEventListener("click", () => {
+    showOverlay();
+})
+
+overlay.addEventListener("click", (event) => {
+    if (event.target === overlay) {
+        hideOverlay();
+    }
+});
+
+cancelFormBtn.addEventListener("click", () => {
+    hideOverlay();
+});
+
+function renderLibrary() {
     gridBox.textContent = "";
 
-    Books.forEach(function (element) {
+    books.forEach((element) => {
         const card = document.createElement("div");
         card.classList.add("card");
 
         const title = document.createElement("h1");
-        title.textContent = element.title;
+        title.textContent = `Title: ${element.name}`;
+        card.appendChild(title);
+
+        const author = document.createElement("p");
+        author.textContent = `Author: ${element.author}`;
+        card.appendChild(author);
 
         const pages = document.createElement("p");
-        pages.textContent = element.pages
-
+        pages.textContent = `Pages: ${element.pages}`;
         card.appendChild(pages);
-        card.appendChild(title);
+
+        const readed = document.createElement("p");
+        readed.textContent = element.read ? "Already read" : "Not read yet";
+        card.appendChild(readed)
+
         gridBox.appendChild(card);
-    });
-}
+    })
+};
 
-function showForm() {
-    overlay.classList.remove("hidden");
-}
+function newBook(name, author, pages, read) {
+    const book = new Book(name, author, pages, read);
+    books.push(book);
+};
 
-newBookBtn.addEventListener("click", showForm);
-
-function cancelForm() {
-    overlay.classList.add("hidden");
-}
-
-cancelFormBtn.addEventListener("click", cancelForm);
-
-function addNewBook(event) {
+form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const newBook = new Book(inputTitle.value, inputPages.value);
-
-    Books.push(newBook);
-    console.log(Books);
-
-    cancelForm();
-    renderBookLibrary();
-}
-
-bookForm.addEventListener("submit", addNewBook);
+    newBook(inputTitle.value, inputAuthor.value, inputPages.value, inputRead.checked);
+    renderLibrary();
+    hideOverlay();
+    console.log(books)
+});
